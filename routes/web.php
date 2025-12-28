c306329af2ea173c7933e88e861ffc3a248c07a9
sonika0295 <sonikakurmi48@gmail.com>
2025-12-28 09:53:12 +0530

    sqs

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InvoiceController;
+use App\Jobs\SendEmailJob;
 
 Route::get('/', function () {
     return redirect()->route('invoices.index');
@@ -14,3 +15,8 @@ Route::resource('customers', CustomerController::class);
 // Invoice Routes
 Route::get('invoices/{id}/download-pdf', [InvoiceController::class, 'downloadPdf'])->name('invoices.download-pdf');
 Route::resource('invoices', InvoiceController::class);
+Route::get('/test-sqs', function () {
+    SendEmailJob::dispatch();
+    return 'Job pushed to SQS';
+});
+
