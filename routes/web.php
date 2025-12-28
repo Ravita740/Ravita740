c64f1d5e39baa6038893a26f49e8558a48588f18
sonika0295 <sonikakurmi48@gmail.com>
2025-12-28 10:39:02 +0530

    s3

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InvoiceController;
 use App\Jobs\SendEmailJob;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return redirect()->route('invoices.index');
@@ -18,5 +19,13 @@ Route::resource('invoices', InvoiceController::class);
 Route::get('/test-sqs', function () {
     SendEmailJob::dispatch();
     return 'Job pushed to SQS';
+});
+Route::get('/upload-test', function () {
+    Storage::disk('s3')->put(
+        'test/hello.txt',
+        'Hello from Laravel S3'
+    );
+
+    return 'File uploaded to S3';
 });
 
